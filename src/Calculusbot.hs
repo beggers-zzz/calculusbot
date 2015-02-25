a3555fae0bc8b1bf7f996ea1ed24fe4dc04b4eb8
Ben Eggers <beggerss@cs.washington.edu>
2015-02-25 00:47:05 -0800

    all the datatypes!

--- src/Calculusbot.hs
+++ src/Calculusbot.hs
@@ -1,12 +1,25 @@
-
 module Calculusbot where
 
 data Expr = Var String
-          | NumLit Integer
+          | Const Const
           | BinExpr BinOp Expr Expr
+          | Sin Expr
+          | Cos Expr
+          | Tan Expr
+          | Sinh Expr
+          | Cosh Expr
+          | Tanh Expr
+          | Log Expr Expr
           deriving (Show)
+
 data BinOp = Plus
            | Minus
            | Times
            | Divide
+           | Power
+           deriving (Show)
+
+data Const = E
+           | Pi
+           | IntLit Integer
            deriving (Show)
