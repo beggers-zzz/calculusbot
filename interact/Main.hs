4dc40a6b7f6cdc77808c8e1871ad2391f85ae33c
Ben Eggers <beggerss@cs.washington.edu>
2015-02-24 23:39:15 -0800

    once more, with parsec!

--- interact/Main.hs
+++ interact/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import qualified Calculusbot as CB
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
 
 main = print getExpr
 
