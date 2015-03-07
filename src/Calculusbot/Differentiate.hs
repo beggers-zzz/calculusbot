ea8db65e4168a17f5674c547becf41a38b306810
Ben Eggers <beggerss@cs.washington.edu>
2015-03-07 00:28:54 -0800

    power rule

--- src/Calculusbot/Differentiate.hs
+++ src/Calculusbot/Differentiate.hs
@@ -16,13 +16,16 @@ dBinOp :: BinOp -> CBExpr -> CBExpr -> CBExpr
 dBinOp Plus l r = (BinExpr Plus (d l) (d r))
 dBinOp Minus l r = (BinExpr Minus (d l) (d r))
 dBinOp Times l r = productRule l r
-dBinOp Divide l r = quotientRule l r
-dBinOp op l r = (BinExpr op l r)  -- temporary cop-out to get stuff mostly working
+dBinOp Divide top bot = quotientRule top bot
+dBinOp Power b e = powerRule b e
 
 dUnOp :: UnOp -> CBExpr -> CBExpr
 dUnOp Sin e = (BinExpr Times (d e) (UnExpr Cos e))
 dUnOp Cos e = (BinExpr Times (d e) (UnExpr Neg (UnExpr Sin e)))
-
+dUnOp Tan e = (BinExpr Divide (d e) (BinExpr Power (UnExpr Cos e) (Const (IntLit 2))))
+dUnOp Sinh e = (BinExpr Times (d e) (UnExpr Cosh e))
+dUnOp Cosh e = (BinExpr Times (d e) (UnExpr Sinh e))
+dUnOp Tanh e = (BinExpr Minus (Const (IntLit 1)) (BinExpr Power (UnExpr Tanh e) (Const (IntLit 2))))
 dUnOp Neg e = (UnExpr Neg (d e))
 dUnOp op e = (UnExpr op e)
 
@@ -30,21 +33,44 @@ productRule :: CBExpr -> CBExpr -> CBExpr
 productRule l r = (BinExpr Plus (BinExpr Times (d l) r) (BinExpr Times l (d r)))
 
 quotientRule :: CBExpr -> CBExpr -> CBExpr
-quotientRule l r = (BinExpr Divide
-                        (BinExpr Minus
-                            (BinExpr Times
-                                r
-                                (d l)
+quotientRule top bot = (BinExpr Divide
+                            (BinExpr Minus
+                                (BinExpr Times
+                                    bot
+                                    (d top)
+                                )
+                                (BinExpr Times
+                                    top
+                                    (d bot)
+                                )
                             )
-                            (BinExpr Times
-                                l
-                                (d r)
+                            (BinExpr Power
+                                bot
+                                (Const (IntLit 2))   -- "magic number" my ass
                             )
                         )
-                        (BinExpr Power
-                            r
-                            (Const (IntLit 2))   -- "magic number" my ass
+
+-- http://en.wikipedia.org/wiki/Differentiation_rules#Generalized_power_rule
+powerRule :: CBExpr -> CBExpr -> CBExpr
+powerRule b e = (BinExpr Times
+                    (BinExpr Power
+                        b
+                        (BinExpr Minus
+                            e
+                            (Const (IntLit 1))
                         )
                     )
-
-
+                    (BinExpr Plus
+                        (BinExpr Times
+                            e
+                            (d b)
+                        )
+                        (BinExpr Times
+                            (BinExpr Times
+                                (UnExpr Log b)
+                                b
+                            )
+                            (d e)
+                        )
+                    )
+                )
